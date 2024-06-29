d2af7a31eb4ac3a37f036fa23c26ab34b2da9b63
Rajmayer <rohitrajmayer22@gmail.com>
2024-06-29 21:46:55 +0700

    final update

---
+++ script.js
@@ -0,0 +1,6 @@
+//========= Navbar Function 
+window.addEventListener("scroll", function(){
+  const header = document.querySelector('header');
+  header.classList.toggle("sticky", window.scrollY > 0);
+})
+
